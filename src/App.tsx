526d3cb4e9664b81ee6f49d0b02a5bdb3560123a
Debora Souza <debora_rss@hotmail.com>
2020-07-07 17:42:36 -0300

    Create Dashboard and Repository routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-function App() {
-  return <h1>Débora</h1>;
-}
-
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </>
+);
 export default App;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Repository: React.FC = () => <div>Repository</div>;
+
+export default Repository;

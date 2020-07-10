6d7c27d599e0979a9ae8065b46d8d79087cfcd42
Debora Souza <debora_rss@hotmail.com>
2020-07-10 18:16:41 -0300

    Consume Github Api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,63 +1,62 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(e: FormEvent): Promise<void> {
+    e.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explores" />
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <input placeholder="Digite aqui" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite aqui"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/22102668?s=460&u=78ccebfddee4a7a14663bb43a43874fbb4fb62c5&v=4"
-            alt="Débora Silva"
-          />
-          <div>
-            <strong>iHelp</strong>
-            <p>
-              App to help people who cannot leave home because of the
-              coronavirus
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/22102668?s=460&u=78ccebfddee4a7a14663bb43a43874fbb4fb62c5&v=4"
-            alt="Débora Silva"
-          />
-          <div>
-            <strong>iHelp</strong>
-            <p>
-              App to help people who cannot leave home because of the
-              coronavirus
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/22102668?s=460&u=78ccebfddee4a7a14663bb43a43874fbb4fb62c5&v=4"
-            alt="Débora Silva"
-          />
-          <div>
-            <strong>iHelp</strong>
-            <p>
-              App to help people who cannot leave home because of the
-              coronavirus
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
